63ee61bdc81fe7e6ba4b4ba7f4dde16e7dfae0ee
MariaLuizaDMoura <malumoura10@gmail.com>
2021-12-12 18:22:15 -0300

    MODIFICAÇÃO DA POSIÇÃO DO TOKEN NO USUARIOSERVICE

--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -55,14 +55,15 @@ public class UsuarioService {
 
 		if (usuario.isPresent()) {
 			if (compararSenhas(usuarioLogin.get().getSenha(), usuario.get().getSenha())) {
-
+				
+				usuarioLogin.get()
+				.setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setSenha(usuario.get().getSenha());
 				usuarioLogin.get().setTipo(usuario.get().getTipo());
-				usuarioLogin.get()
-						.setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+
 
 				return usuarioLogin;
 
